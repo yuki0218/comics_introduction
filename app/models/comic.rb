63c66d7a7a18da565398f7f32dd69eba1a52c178
yuki0218 <ktnyk0305@gmail.com>
2022-08-15 18:36:47 +0900

    comicモデルのバリデーション

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,2 +1,8 @@
 class Comic < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :volume, presence: true
+  validates :completion, presence: true
+
+  belongs_to :user
 end
