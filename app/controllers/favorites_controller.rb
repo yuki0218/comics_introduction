dea7430bfb26289af38e582b853aaa479ebf3987
yuki0218 <ktnyk0305@gmail.com>
2022-08-28 02:47:02 +0900

    いいね機能の非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,20 @@
 class FavoritesController < ApplicationController
+  before_action :comic_params, only: [:create, :destroy]
+
   def create
-    @favorite = Favorite.create(user_id: current_user.id, comic_id: params[:comic_id])
-    redirect_back fallback_location: root_path
+    @favorite = Favorite.create(user_id: current_user.id, comic_id: @comic.id)
+    # redirect_back fallback_location: root_path
   end
 
   def destroy
-    @favorite = Favorite.find_by(user_id: current_user.id, comic_id: params[:comic_id])
+    @favorite = Favorite.find_by(user_id: current_user.id, comic_id: @comic.id)
     @favorite.destroy
-    redirect_back fallback_location: root_path
+    # redirect_back fallback_location: root_path
+  end
+
+  private
+
+  def comic_params
+    @comic = Comic.find(params[:comic_id])
   end
 end
